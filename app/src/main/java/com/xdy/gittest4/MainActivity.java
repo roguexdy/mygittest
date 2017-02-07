1782e422e047cb81801b5ddbd59d7df3aa99552a
roguexdy <rogue_xdy@163.com>
2017-02-07 10:13:46 +0800

    第三次

--- app/src/main/java/com/xdy/gittest4/MainActivity.java
+++ app/src/main/java/com/xdy/gittest4/MainActivity.java
@@ -2,6 +2,8 @@ package com.xdy.gittest4;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        VideoView videoView = new VideoView(this);
+        SeekBar seekBar = new SeekBar(this);
     }
 }
