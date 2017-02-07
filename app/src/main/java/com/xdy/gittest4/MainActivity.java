c075e48bd9312e8433700185cd4b0931c562a36e
roguexdy <rogue_xdy@163.com>
2017-02-07 10:49:15 +0800

    内容撤回

--- app/src/main/java/com/xdy/gittest4/MainActivity.java
+++ app/src/main/java/com/xdy/gittest4/MainActivity.java
@@ -1,13 +1,7 @@
 package com.xdy.gittest4;
 
-import android.support.v4.view.NestedScrollingChild;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.HorizontalScrollView;
-import android.widget.ImageView;
-import android.widget.ScrollView;
-import android.widget.TextView;
-import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,63 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView textView = new TextView(this);
-
-        init();
-    }
-
-    private void init() {
-        TextView textView = new TextView(this);
-        ImageView imageView = new ImageView(this);
-        VideoView videoView = new VideoView(this);
-        ScrollView scrollView = new ScrollView(this);
-
-        HorizontalScrollView horizontalScrollView = new HorizontalScrollView(this);
-        NestedScrollingChild nestedScrollingChild = new NestedScrollingChild() {
-            @Override
-            public void setNestedScrollingEnabled(boolean enabled) {
-
-            }
-
-            @Override
-            public boolean isNestedScrollingEnabled() {
-                return false;
-            }
-
-            @Override
-            public boolean startNestedScroll(int axes) {
-                return false;
-            }
-
-            @Override
-            public void stopNestedScroll() {
-
-            }
-
-            @Override
-            public boolean hasNestedScrollingParent() {
-                return false;
-            }
-
-            @Override
-            public boolean dispatchNestedScroll(int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed, int[] offsetInWindow) {
-                return false;
-            }
-
-            @Override
-            public boolean dispatchNestedPreScroll(int dx, int dy, int[] consumed, int[] offsetInWindow) {
-                return false;
-            }
-
-            @Override
-            public boolean dispatchNestedFling(float velocityX, float velocityY, boolean consumed) {
-                return false;
-            }
-
-            @Override
-            public boolean dispatchNestedPreFling(float velocityX, float velocityY) {
-                return false;
-            }
-        };
     }
 }
