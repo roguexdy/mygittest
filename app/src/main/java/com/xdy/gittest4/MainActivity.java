90438bdd3ca06ab3ab07d579eefe5ecd6d29d54d
roguexdy <rogue_xdy@163.com>
2017-02-07 10:41:34 +0800

    main修改

--- app/src/main/java/com/xdy/gittest4/MainActivity.java
+++ app/src/main/java/com/xdy/gittest4/MainActivity.java
@@ -2,7 +2,10 @@ package com.xdy.gittest4;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         TextView textView = new TextView(this);
+
+        init();
+    }
+
+    private void init() {
+        TextView textView = new TextView(this);
+        ImageView imageView = new ImageView(this);
+        VideoView videoView = new VideoView(this);
+        ScrollView scrollView = new ScrollView(this);
     }
 }
