c127e5c0d9f6a9f3e72a7ddc9c7afb5591234d4f
roguexdy <rogue_xdy@163.com>
2017-02-07 10:46:51 +0800

    init添加内容

--- app/src/main/java/com/xdy/gittest4/MainActivity.java
+++ app/src/main/java/com/xdy/gittest4/MainActivity.java
@@ -1,7 +1,9 @@
 package com.xdy.gittest4;
 
+import android.support.v4.view.NestedScrollingChild;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.HorizontalScrollView;
 import android.widget.ImageView;
 import android.widget.ScrollView;
 import android.widget.TextView;
@@ -23,5 +25,53 @@ public class MainActivity extends AppCompatActivity {
         ImageView imageView = new ImageView(this);
         VideoView videoView = new VideoView(this);
         ScrollView scrollView = new ScrollView(this);
+
+        HorizontalScrollView horizontalScrollView = new HorizontalScrollView(this);
+        NestedScrollingChild nestedScrollingChild = new NestedScrollingChild() {
+            @Override
+            public void setNestedScrollingEnabled(boolean enabled) {
+
+            }
+
+            @Override
+            public boolean isNestedScrollingEnabled() {
+                return false;
+            }
+
+            @Override
+            public boolean startNestedScroll(int axes) {
+                return false;
+            }
+
+            @Override
+            public void stopNestedScroll() {
+
+            }
+
+            @Override
+            public boolean hasNestedScrollingParent() {
+                return false;
+            }
+
+            @Override
+            public boolean dispatchNestedScroll(int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed, int[] offsetInWindow) {
+                return false;
+            }
+
+            @Override
+            public boolean dispatchNestedPreScroll(int dx, int dy, int[] consumed, int[] offsetInWindow) {
+                return false;
+            }
+
+            @Override
+            public boolean dispatchNestedFling(float velocityX, float velocityY, boolean consumed) {
+                return false;
+            }
+
+            @Override
+            public boolean dispatchNestedPreFling(float velocityX, float velocityY) {
+                return false;
+            }
+        };
     }
 }
